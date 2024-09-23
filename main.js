d49f09f9d8df88212258938e12f1cfbeedb84f7a
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2024-09-23 14:01:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 function print_HelloWorld() {
-    let text = "Hello, World";
-    document.getElementById('text').innerHTML = text;
-    console.log(text);
+	let text = "Hello, World";
+	document.getElementById("text").innerHTML = text;
+	console.log(text);
 }
 
 function simpleArithmetic() {
-    let x = 5;
-    let y = 3;
-    return x * y;
+	let x = 5;
+	let y = 3;
+	return x * y;
 }
-
-// document.getElementById('text').innerHTML = simpleArithmetic();
 
 function oddEven(x) {
 	if (isNaN(x)) {
@@ -24,17 +22,12 @@ function oddEven(x) {
 	}
 }
 
-// document.getElementById("text").innerHTML = oddEven(1332);
+function arraySum() {
+	let numbers = [1, 2, 3, 4, 5];
 
-function arraySum(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    return sum;
+	let sum = 0;
+	for (let i = 0; i < numbers.length; i++) {
+		sum += numbers[i];
+	}
+	console.log("Sum:", sum);
 }
-
-// Example usage
-let numbers = [1, 2, 3, 4, 5];
-let sum = arraySum(numbers);
-console.log("Sum:", sum);
