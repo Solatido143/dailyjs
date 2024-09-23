91d9c139618c6129ccd4690d2b9a497c0b1ced9c
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2024-09-23 13:58:21 +0800

    Array Sum
    Given an array of numbers, write a function to find the sum of all the numbers in the array.

--- main.js
+++ main.js
@@ -24,4 +24,17 @@ function oddEven(x) {
 	}
 }
 
-document.getElementById("text").innerHTML = oddEven(1332);
+// document.getElementById("text").innerHTML = oddEven(1332);
+
+function arraySum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+// Example usage
+let numbers = [1, 2, 3, 4, 5];
+let sum = arraySum(numbers);
+console.log("Sum:", sum);
