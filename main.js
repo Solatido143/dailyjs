032eef76a6c42d2a354db1ac8b4e499100e6dc9b
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2024-10-08 17:37:52 +0800

    flatten an array

--- main.js
+++ main.js
@@ -257,3 +257,16 @@ function fibonacci(n) {
 	}
 	return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+function flatten(arr) {
+	const result = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (Array.isArray(arr[i])) {
+			result.push(...flatten(arr[i]));
+		} else {
+			result.push(arr[i]);
+		}
+	}
+	return result;
+}
+console.log(flatten([1, [2, 3], [4, [5]]]));
